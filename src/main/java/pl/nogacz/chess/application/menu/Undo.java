bd1e286cebde27fa3b917d05adc08e23898db346
metinkok <m.metinkok@gmail.com>
2020-12-15 15:08:39 -0500

    Changed getCoordinate method to static for test

--- src/main/java/pl/nogacz/chess/application/menu/Undo.java
+++ src/main/java/pl/nogacz/chess/application/menu/Undo.java
@@ -21,7 +21,7 @@ public class Undo{
         removeMovement(player);
     }
 
-    public Coordinates getCoordinate(String s){
+    public static Coordinates getCoordinate(String s){
         char letter = s.charAt(0);
         int y = Integer.parseInt(s.substring(1));
         int x = -1;
