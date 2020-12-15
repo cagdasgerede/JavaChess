2ae1d440b2a043e0e7f8bf852d7ab74768be0c25
metinkok <m.metinkok@gmail.com>
2020-12-15 15:21:46 -0500

    Added some tests

---
+++ src/test/java/pl/nogacz/chess/board/UndoTest.java
@@ -0,0 +1,34 @@
+package pl.nogacz.chess.board;
+
+import org.junit.Test;
+
+import jdk.jfr.Timestamp;
+
+import org.junit.Assert;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+import pl.nogacz.chess.application.menu.Undo;
+
+public class UndoTest{
+
+    @Test
+    public void testCoordinate(){
+        Coordinates coordinate = new Coordinates(0,0);
+
+        assertEquals(coordinate, Undo.getCoordinate("a8"));
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testNullCoordinate(){
+        Undo.getCoordinate(null);
+        fail();
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void testEmptyString(){
+        Undo.getCoordinate("");
+        fail();
+    }
+}
