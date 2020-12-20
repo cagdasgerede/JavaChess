c28bfb7456e838d2a82163dd72e5c645f09a85e6
talhaunal <t.unal@etu.edu.tr>
2020-12-20 16:50:31 +0300

    Exceptions logged to logs.txt.

--- src/main/java/pl/nogacz/chess/application/SoundEffect.java
+++ src/main/java/pl/nogacz/chess/application/SoundEffect.java
@@ -2,6 +2,8 @@ package pl.nogacz.chess.application;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -9,35 +11,45 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-	//https://gist.github.com/figengungor/5673813 Helped from this link.
+//https://gist.github.com/figengungor/5673813 Helped from this link.
 public class SoundEffect {	
 	private Clip clip;
+	Logger logger;
+	FileHandler handler;
+	
 	public SoundEffect(String soundFileName) {
 		try {
-	    	File file = new File(soundFileName);
+		    logger = Logger.getLogger(SoundEffect.class.getName()); 
+			handler = new FileHandler("logs.txt",true); 
+			logger.addHandler(handler); 
+			logger.setLevel(java.util.logging.Level.WARNING);
+			
+			File file = new File(soundFileName);
 	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
 	        clip = AudioSystem.getClip();
 	    	clip.open(audioInputStream);
 	    } catch (UnsupportedAudioFileException e) {
-	        e.printStackTrace();
+			logger.warning(e.getMessage());
 	    } catch (IOException e) {
-	        e.printStackTrace();
+			logger.warning(e.getMessage());
 	    } catch (LineUnavailableException e) {
-	        e.printStackTrace();
-	    }
+			logger.warning(e.getMessage());
+		}
+		handler.close();
 	}
  
 	public void play(Boolean loop) { // Play or Re-play the sound effect from the beginning, by rewinding.
-		if (clip.isRunning())
-	    	clip.stop();   // Stop the player if it is still running
-	        clip.setFramePosition(0); // Rewind to the beginning
-	        clip.start();     // Start playing
-	        if(loop)//Loop if loop parameter is true
-		    	clip.loop(Clip.LOOP_CONTINUOUSLY);
-	   }
+		if (clip.isRunning()){
+			clip.stop();   // Stop the player if it is still running
+		}
+	    clip.setFramePosition(0); // Rewind to the beginning
+	    clip.start();     // Start playing
+	    if(loop){ //Loop if loop parameter is true
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		}
+	}
 	   	   
-	public void stop() //Stop playing and rewind to be played again from the beginning
-	{
+	public void stop(){ //Stop playing and rewind to be played again from the beginning
 		clip.stop();
 		clip.setFramePosition(0);
 	}	  
